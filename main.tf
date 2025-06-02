905093cf0516241e970481b6b916070f8e526907
Sharmila Nagarajan1 <sharmeeh@outlook.com.au>
2025-06-02 08:16:57 -0400

    commit3

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ data "azurerm_resource_group" "rg" {
 
 resource "azurerm_storage_account" "stacc" {
   name                     = "azurepipelineGithubAc"
-  resource_group_name      = data.azurerm_resource_group.example.name  
+  resource_group_name      = data.azurerm_resource_group.rg.name  
   location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
