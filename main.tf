bebbca7010b1b0abc500a59a19600268ea6f8b85
Sharmila Nagarajan1 <sharmeeh@outlook.com.au>
2025-06-02 08:07:55 -0400

    commit2

--- main.tf
+++ main.tf
@@ -4,13 +4,13 @@ provider "azurerm" {
 
 data "azurerm_resource_group" "rg" {
   name     = "kml_rg_main-f5fc139ce6224d3a"
-  location = "westus"
+  
 }
 
 resource "azurerm_storage_account" "stacc" {
   name                     = "azurepipelineGithubAc"
-  resource_group_name      = data.azurerm_resource_group.example.name
-  location                 = data.azurerm_resource_group.example.location
+  resource_group_name      = data.azurerm_resource_group.example.name  
+  location                 = data.azurerm_resource_group.rg.location
   account_tier             = "Standard"
   account_replication_type = "GRS"
 
