b4212cdfb1ce27e614737ecbc956ed02e65faa8f
Sharmila Nagarajan <sharmeeh@outlook.com.au>
2025-06-02 22:26:36 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,10 @@
 provider "azurerm" {
   features {}
+      client_id          = "2eaf4b0b-e287-445d-ae0f-1da2e3ea808e"
+      client_secret       = "Kof8Q~hHpfkAbXXo~EXPaYzd9uzP3uoeZlp5hdb8"
+      subscription_id     ="a2b28c85-1948-4263-90ca-bade2bac4df4"
+      tenant_id           ="30fe8ff1-adc6-444d-ba94-1238894df42c"
+
 }
 
 data "azurerm_resource_group" "rg" {
